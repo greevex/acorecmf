7835f9ba7eae01189d8a935667650393fdfb46b8
axel.90@inbox.ru <axel.90@inbox.ru@a14f3080-5592-11de-8ef9-cf35a3c3eb08>
2009-07-08 11:21:26 +0000

    Добавил в панель редактор глоб.модулей.

--- core/system/global_mod.php
+++ core/system/global_mod.php
@@ -1,12 +1,12 @@
 <?php
 function setGlobalMod(){
-	
+
 	$url = explode("/", substr(str_replace(str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT), '', $_SERVER['REDIRECT_URL']), 1));
 	if (count($url) == 0) return;
 	if ($url[0] == "manager") return;
-	
+
 	$globs = ACore::decode(ROOT . "/core/config/global.php");
-	
+
 	while (count($url) > 0){
 		$now = implode("/", $url);
 		if (isset($globs[$now])){
@@ -23,8 +23,11 @@ function setGlobalMod(){
 	}
 	$now = "";
 	if (isset($globs[$now])){
+		if ($globs[$now] == ""){
+			define("GLOBAL_STR", $now);
+			return;
+		}
 		define("GLOBAL_MOD", $globs[$now]);
 		include(ROOT . "/modules/{$globs[$now]}/{$globs[$now]}.php");
 	}
 }
-?>
